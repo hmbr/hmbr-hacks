705a63981696ebee766647c4c75944906dd66190
helder <helderribas@gmail.com>
2010-05-05 22:55:26 -0300

    new script

---
+++ twitterListagemNotFollowers.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import twitter
+import ConfigParser
+
+config_file = "lazy.cfg"
+config = ConfigParser.RawConfigParser()
+config.read(config_file)
+api = twitter.Api(username=config.get("twitter","username"),password=config.get("twitter","password")  )
+    
+followers = set(map(lambda x : x.screen_name,api.GetFollowers()))
+friends = set(map(lambda x : x.screen_name,api.GetFriends())) 
+   
+print " Who I follow but not follow me"
+for user in friends:
+    if user not in followers:
+        print " ", user
+
+print " Who follow me but I not follow "
+for user in followers:
+    if user not in friends:
+        print " ", user
