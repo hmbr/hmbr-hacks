b74f72880445b0150baab44531472b9c89c7d7e4
helder <helderribas@gmail.com>
2010-05-06 02:16:40 -0300

    command line

--- twitterListagemNotFollowers.py
+++ twitterListagemNotFollowers.py
@@ -2,21 +2,35 @@
 
 import twitter
 import ConfigParser
+import getopt
+import sys
 
-config_file = "user.cfg"
-config = ConfigParser.RawConfigParser()
-config.read(config_file)
-api = twitter.Api(username=config.get("twitter","username"),password=config.get("twitter","password")  )
-    
-followers = map(lambda x : x.screen_name,api.GetFollowers())
-friends = map(lambda x : x.screen_name,api.GetFriends())
-   
-print " Who I follow but not follow me"
-for user in friends:
-    if user not in followers:
-        print " ", user
+def main():
+	config_file = "user.cfg"
+	try:
+		opts,args = getopt.getopt(sys.argv[1:],"c:",["conf"])
+	except getopt.GetoptError, err:
+		print str(err)
+		
+	for o, a in opts:
+		if o in ("-c", "--conf"):
+			config_file = a
 
-print " Who follow me but I not follow "
-for user in followers:
-    if user not in friends:
-        print " ", user
+	config = ConfigParser.RawConfigParser()
+	config.read(config_file)
+	
+	api = twitter.Api(username=config.get("twitter","username"),password=config.get("twitter","password")  )
+	
+	followers = map(lambda x : x.screen_name,api.GetFollowers())
+	friends = map(lambda x : x.screen_name,api.GetFriends())
+	print " Who I follow but not follow me"
+	for user in friends:
+		if user not in followers:
+			print " ", user
+	print " Who follow me but I not follow "
+	for user in followers:
+		if user not in friends:
+			print " ", user
+
+if __name__ == "__main__":
+	main()
